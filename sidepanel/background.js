9eaf9d5cfa970f349d2f51227c712e7402b1ae73
tienvu95 <tienvu95@gmail.com>
2023-08-30 19:25:55 +0700

    update side panel

---
+++ sidepanel/background.js
@@ -0,0 +1,35 @@
+// background.js
+
+// Function to toggle the ChatGPT popup window
+function toggleChatbotWindow() {
+  const chatbotUrl = "https://chat.openai.com";
+  let chatbotWindow = null;
+
+  chrome.windows.getAll({ populate: true }, (windows) => {
+    windows.forEach((win) => {
+      win.tabs.forEach((tab) => {
+        if (tab.url === chatbotUrl) {
+          chatbotWindow = win.id;
+        }
+      });
+    });
+
+    if (chatbotWindow) {
+      chrome.windows.update(chatbotWindow, { focused: true });
+    } else {
+      chrome.windows.create({
+        url: chatbotUrl,
+        type: "popup",
+        width: 400,
+        height: 600,
+      });
+    }
+  });
+}
+
+// Listen for messages from the content script
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "toggle_chatbot") {
+    toggleChatbotWindow();
+  }
+});
---
+++ sidepanel/popup.js
@@ -0,0 +1,10 @@
+// popup.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Remove the event listener for the previous chatbotButton
+  // document.getElementById("chatbotButton").addEventListener("click", function () {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  //     chrome.tabs.sendMessage(tabs[0].id, { action: "open_chatbot" });
+  //   });
+  // });
+});
